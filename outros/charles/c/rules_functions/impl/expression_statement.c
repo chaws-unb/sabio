1f7e1da92af83a5fe1eab8b84bbc645091e0a9ec
Chaws <chaws@chaws-pc.(none)>
2013-07-23 00:46:31 -0400

    Implementando as funcoes para conseguir o eval do if

--- outros/charles/c/rules_functions/impl/expression_statement.c
+++ outros/charles/c/rules_functions/impl/expression_statement.c
@@ -3,16 +3,29 @@
 #include <string.h>
 #include "../cGrammar.h"
 
-
+// THIS MY FRIEND IS AN EMPTY STATEMENT!
 void * expression_statement__SEMICOLON()
 {
-	
+	// Make this node as the next step of previous AST
+	mainProgram->next = newProgramNode(NULL);
+
+	// Now, the current tree is this node
+	// Remember the first tree is stored in main.c for later evaluation hehe
+	mainProgram = mainProgram->next;
 }
 
 
 void * expression_statement__expression__SEMICOLON(ast * expr)
 {
 	if(debugMode) printf(" : expression_statement->expression ;\n");
+
+	// Make this node as the next step of previous AST
+	mainProgram->next = newProgramNode(expr);
+
+	// Now, the current tree is this node
+	// Remember the first tree is stored in main.c for later evaluation hehe
+	mainProgram = mainProgram->next;
+
 	return expr;
 }
 
